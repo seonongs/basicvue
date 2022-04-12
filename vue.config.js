ade8218e6fae6a636db58979a4b1a121754e537c
seonongs <seonong@gmail.com>
2022-04-12 19:55:57 +0900

    changed

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    devServer: {
+        open: true,//
+        port: 8888,//
+        // proxy: {
+        //     //
+        //     '/axios': {
+        //         target: 'http://101.15.22.98',
+        //         changeOrigin: true,
+        //         secure: false, //   http  ，
+        //         pathRewrite: {
+        //             '^/axios': ''
+        //         }
+        //     }
+        // }
+    }
+}
+
