8f0c40c94760d1c74c22fc7a9f8a0912986dfdf7
seonongs <seonong@gmail.com>
2022-04-14 14:38:07 +0900

    changed

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:8080';
+// axios.defaults.baseURL = 'http://49.50.173.46:8080';
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 function fetchUserList() {
     return axios.get(`/api/user`)
 }
